a1908f908a3b9f0d4e195a0e978fc7b75c8f97f2
nidhi-2286 <73793406+nidhi-2286@users.noreply.github.com>
2021-07-18 15:44:29 +0530

    Update TSF Task 1..py

--- TSF Task 1..py
+++ TSF Task 1..py
@@ -21,19 +21,9 @@ data_load
 
 # # preprocessing
 
-# In[34]:
-
-
 data_load.info()
 
-
-# In[ ]:
-
-
 data_load.describe()
-
-
-# In[22]:
 
 
 data_load.plot(x='Hours', y='Scores', style='o')
@@ -43,13 +33,7 @@ plt.ylabel('Percentage Scored')
 plt.show()
 
 
-# In[36]:
-
-
 data_load.isnull().sum()
-
-
-# In[37]:
 
 
 data_load.mean()
@@ -57,24 +41,13 @@ data_load.mean()
 
 # # spliting the dataset
 
-# In[38]:
-
-
 X = data_load.iloc[:, :-1].values
 y = data_load.iloc[:, 1].values
 print("shape of x:", X.shape)
 print("shape of y:", y.shape)
 
-
-# In[24]:
-
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0) 
-
-
-# In[39]:
-
 
 print("shape of X train:", X_train.shape)
 print("shape of X_test:", X_test.shape)
@@ -82,49 +55,23 @@ print("shape of y:", y_train.shape)
 print("shape of y:", y_test.shape)
 
 
+
 # # selecting and training the model
-
-# In[ ]:
-
-
-
-
-
-# In[44]:
-
 
 from sklearn.linear_model import LinearRegression 
 regressor = LinearRegression()
 
-
-# In[45]:
-
-
 regressor.fit(X_train ,y_train)
-
-
-# In[48]:
-
 
 regressor.coef_
 
-
-# In[49]:
-
-
 regressor.intercept_
-
-
-# In[51]:
-
 
 y_pred = regressor.predict(X_test)
 y_pred
 
 
 # # plotting
-
-# In[26]:
 
 
 line = regressor.coef_*X+regressor.intercept_
@@ -133,31 +80,18 @@ plt.plot(X,line);
 plt.show()
 
 
-# In[54]:
-
-
 df = pd.DataFrame(np.c_[X_test, y_test, y_pred], columns = ["Study Hours", "students_original_marks", "Students_predicted_marks"])
 df
 
 
 # # accuracy
 
-# In[55]:
-
-
 regressor.score(X_test, y_test)
 
 
 # # plotting
 
-# In[56]:
-
-
 plt.scatter(X_test, y_test)
-
-
-# In[57]:
-
 
 plt.scatter(X_test, y_test)
 plt.plot(X_train, regressor.predict(X_train), color="red")
@@ -165,16 +99,11 @@ plt.plot(X_train, regressor.predict(X_train), color="red")
 
 # # presenting solution
 
-# In[58]:
-
-
 hours = [[2]]
 own_pred = regressor.predict(hours)
 print("Number of hours = {}".format(hours))
 print("Predicted Score = {}".format(own_pred[0]))
 
-
-# In[ ]:
 
 
 
